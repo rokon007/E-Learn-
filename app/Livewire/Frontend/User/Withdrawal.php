a2ae4ee2466ccebfeb4c8d9242d51a8bad435d5a
@Rokon*77@ <rezaulkrn85@gmail.com>
2025-09-18 20:16:32 +0600

    sjhgkjfdghjkd

--- app/Livewire/Frontend/User/Withdrawal.php
+++ app/Livewire/Frontend/User/Withdrawal.php
@@ -8,6 +8,6 @@ class Withdrawal extends Component
 {
     public function render()
     {
-        return view('livewire.frontend.user.withdrawal');
+        return view('livewire.frontend.user.withdrawal')->layout('livewire.layout.user-app');
     }
 }
