5e987eeb661e8f8ace87113721ed88d7c4250884
@Rokon*77@ <rezaulkrn85@gmail.com>
2025-09-18 22:10:03 +0600

    pymentMetod image

--- resources/views/livewire/frontend/user/withdrawal.blade.php
+++ resources/views/livewire/frontend/user/withdrawal.blade.php
@@ -10,6 +10,41 @@
     <div class="dashboard-header">
         <h1 class="dashboard-title">Withdrawal</h1>
     </div>
+    <div class="dashboard-cards">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Blance</h3>
+                <div class="card-icon">
+                    ৳
+                </div>
+            </div>
+            <div class="card-value">140.2 ৳</div>
+            <p class="card-text">Your withdrawal Account is Active,You can withdraw only after 7 days from your last withdrawal!</p>
+        </div>
+    </div>
+    <div class="dashboard-cards">
+        <div class="card">
+            <img src="{{asset('assets/img/payment/bikash.png')}}" alt="bikash"/>
+        </div>
+        <div class="card">
+            <img src="{{asset('assets/img/payment/bikash.png')}}" alt="bikash"/>
+        </div>
+        <div class="card">
+            <img src="{{asset('assets/img/payment/bikash.png')}}" alt="bikash"/>
+        </div>
+        <div class="card">
+            <img src="{{asset('assets/img/payment/bikash.png')}}" alt="bikash"/>
+        </div>
+        <div class="card">
+            <img src="{{asset('assets/img/payment/bikash.png')}}" alt="bikash"/>
+        </div>
+        <div class="card">
+            <img src="{{asset('assets/img/payment/bikash.png')}}" alt="bikash"/>
+        </div>
+        <div class="card">
+            <img src="{{asset('assets/img/payment/bikash.png')}}" alt="bikash"/>
+        </div>
+    </div>
    @section('js')
 
    @endsection
