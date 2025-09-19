9adbc22fded736a60c5228518a07deb2bd1c14c5
@Rokon*77@ <rezaulkrn85@gmail.com>
2025-09-19 18:22:00 +0600

    jgfkfd554

--- resources/views/livewire/layout/user-aside.blade.php
+++ resources/views/livewire/layout/user-aside.blade.php
@@ -3,17 +3,75 @@
         <h3>User Dashboard</h3>
     </div>
     <ul class="sidebar-menu">
-        <li><a href="{{route('dashboard')}}" class="active"><i class="fas fa-th-large"></i> Dashboard</a></li>
-        <li><a href="/" class=""><i class="fas fa-home"></i> Home</a></li>
-        <li><a href="{{route('profile')}}"><i class="fas fa-user"></i> My Profile</a></li>
-        <li><a href="{{route('leadconverts')}}"><i class="fas fa-exchange-alt"></i> Lead Convert</a></li>
-        <li><a href="{{route('passbook')}}"><i class="fas fa-wallet"></i> PassBook</a></li>
-        <li><a href="{{route('courses')}}"><i class="fas fa-book"></i> Courses</a></li>
-        <li><a href="{{route('referral')}}"><i class="fas fa-users"></i> Referral</a></li>
-        <li><a href="{{route('withdrawal')}}"><i class="fas fa-money-bill-wave"></i> Withdrawal</a></li>
-        <li><a href="{{route('changepassword')}}"><i class="fas fa-key"></i> Change Password</a></li>
-        <li><a href="{{route('homework')}}"><i class="fas fa-tasks"></i> Homework</a></li>
-        <li><a href="{{route('buyoffer')}}"><i class="fas fa-gift"></i> Buy Offer</a></li>
-        <li><a style="cursor: pointer;"  wire:click="$dispatch('logout')"><i class="fas fa-lock"></i> Logout</a></li>
+        <li>
+            <a href="{{route('dashboard')}}"
+               class="{{ request()->routeIs('dashboard') ? 'active' : '' }}">
+               <i class="fas fa-th-large"></i> Dashboard
+            </a>
+        </li>
+        <li>
+            <a href="/" class="{{ request()->is('/') ? 'active' : '' }}">
+                <i class="fas fa-home"></i> Home
+            </a>
+        </li>
+        <li>
+            <a href="{{route('profile')}}"
+               class="{{ request()->routeIs('profile') ? 'active' : '' }}">
+               <i class="fas fa-user"></i> My Profile
+            </a>
+        </li>
+        <li>
+            <a href="{{route('leadconverts')}}"
+               class="{{ request()->routeIs('leadconverts') ? 'active' : '' }}">
+               <i class="fas fa-exchange-alt"></i> Lead Convert
+            </a>
+        </li>
+        <li>
+            <a href="{{route('passbook')}}"
+               class="{{ request()->routeIs('passbook') ? 'active' : '' }}">
+               <i class="fas fa-wallet"></i> PassBook
+            </a>
+        </li>
+        <li>
+            <a href="{{route('courses')}}"
+               class="{{ request()->routeIs('courses') ? 'active' : '' }}">
+               <i class="fas fa-book"></i> Courses
+            </a>
+        </li>
+        <li>
+            <a href="{{route('referral')}}"
+               class="{{ request()->routeIs('referral') ? 'active' : '' }}">
+               <i class="fas fa-users"></i> Referral
+            </a>
+        </li>
+        <li>
+            <a href="{{route('withdrawal')}}"
+               class="{{ request()->routeIs('withdrawal') ? 'active' : '' }}">
+               <i class="fas fa-money-bill-wave"></i> Withdrawal
+            </a>
+        </li>
+        <li>
+            <a href="{{route('changepassword')}}"
+               class="{{ request()->routeIs('changepassword') ? 'active' : '' }}">
+               <i class="fas fa-key"></i> Change Password
+            </a>
+        </li>
+        <li>
+            <a href="{{route('homework')}}"
+               class="{{ request()->routeIs('homework') ? 'active' : '' }}">
+               <i class="fas fa-tasks"></i> Homework
+            </a>
+        </li>
+        <li>
+            <a href="{{route('buyoffer')}}"
+               class="{{ request()->routeIs('buyoffer') ? 'active' : '' }}">
+               <i class="fas fa-gift"></i> Buy Offer
+            </a>
+        </li>
+        <li>
+            <a style="cursor: pointer;" wire:click="$dispatch('logout')">
+               <i class="fas fa-lock"></i> Logout
+            </a>
+        </li>
     </ul>
 </aside>
